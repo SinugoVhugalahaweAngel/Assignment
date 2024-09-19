4c1e3092bea61426255345e106ed479652a36bb1
RC_Student_lab <RC_Student_lab@labG9AEB3.phdvvtwzx4oudioi2mg3lhtmwg.fx.internal.cloudapp.net>
2024-09-19 23:17:58 +0000

    Add User class with attributes for username, password, first name, and  last name

---
+++ src/main/java/com/mycompany/useraccountmanager/User.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.useraccountmanager;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+
+//// Class to store user informaion with attributes for username, password, first name, and last name
+
+public class User{
+    // Private variables to store user details
+   
+    private String username;
+     private String password;
+     private String firstname;
+     private String Lastname;
+     
+    
+}
